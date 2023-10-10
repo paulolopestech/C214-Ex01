ca71f268d20e858f7a24562c83ce78b020b99eab
paulolopestech <phlj.junior@gmail.com>
2023-10-09 23:57:33 -0300

    :test_tube: testing format, show and filter

--- test.ts
+++ test.ts
@@ -1,21 +1,81 @@
 import { MockServer, MockServerOffline } from "./MockServer";
 import { Schedule } from "./Schedule";
 
-describe('Testing get server data', () => {
-    test('should accept a valid string returned from server', async () => {
+const mockSchedule = [JSON.parse('{"nomeDoProfessor":"Chris","horarioDeAtendimento":"Segunda-Feira 10:30","periodo":"Integral","sala":"1","predio":["1"]}')];
+
+describe('Testing getSchedule', () => {
+    test('should accept a string returned from server', async () => {
         const schedule = new Schedule(new MockServer);
         const serverData: string = await schedule.getSchedule();
-        const parsedServerData = JSON.parse(serverData);
-        expect(parsedServerData).toHaveProperty('nomeDoProfessor');
-        expect(parsedServerData).toHaveProperty('horarioDeAtendimento');
-        expect(parsedServerData).toHaveProperty('periodo');
-        expect(parsedServerData).toHaveProperty('sala');
-        expect(parsedServerData).toHaveProperty('predio');
+        const response = JSON.parse(serverData);
+        expect(typeof response).toBe('object')
     });
 
     test('should throw error if server does not respond', async () => {
         const schedule = new Schedule(new MockServerOffline);
-        const serverData: string = await schedule.getSchedule();
-        expect(serverData).toEqual('Error');
+        const response: string = await schedule.getSchedule();
+        expect(response).toEqual('Error');
     });
-})
+});
+
+describe('Testing formatSchedule', () => {
+    test('should return a parsed json', async () => {
+        const input = '{"nomeDoProfessor":"Chris","horarioDeAtendimento":"Segunda-Feira 10:30","periodo":"Integral","sala":"1","predio":["1"]}';
+
+        const schedule = new Schedule(new MockServer);
+        const response: string = await schedule.formatSchedule(input);
+        expect(response).toHaveProperty('nomeDoProfessor');
+        expect(response).toHaveProperty('horarioDeAtendimento');
+        expect(response).toHaveProperty('periodo');
+        expect(response).toHaveProperty('sala');
+        expect(response).toHaveProperty('predio');
+    });
+
+    test('should return invalid input if input is not a string', async () => {
+        const input: any = 10;
+        const schedule = new Schedule(new MockServer);
+        const response: string = await schedule.formatSchedule(input);
+        expect(response).toEqual('Invalid input');
+    });
+});
+
+describe('Testing showSchedule', () => {
+    test('should return a schedule list', async () => {
+        const schedule = new Schedule(new MockServer);
+        schedule.schedule = mockSchedule;
+        const response = schedule.showSchedule();
+        expect(response?.length).toBe(1);
+    });
+
+
+    test('should return null if not get schedule', async () => {
+        const schedule = new Schedule(new MockServer);
+        const response = schedule.showSchedule();
+        expect(response).toBe(null);
+    });
+});
+
+describe('Testing showFilteredSchedule', () => {
+    test('should return empty array if not get schedule', async () => {
+        const schedule = new Schedule(new MockServer);
+        const filter = {building: '6'}
+        const response = schedule.showFilteredSchedule(filter);
+        expect(response).toEqual([]);
+    });
+
+    test('should return empty array if not match filter', async () => {
+        const schedule = new Schedule(new MockServer);
+        schedule.schedule = mockSchedule;
+        const filter = {building: '6'};
+        const response = schedule.showFilteredSchedule(filter);
+        expect(response).toEqual([]);
+    });
+
+    test('should return list of teachers who work in building 1', async () => {
+        const schedule = new Schedule(new MockServer);
+        schedule.schedule = mockSchedule;
+        const filter = {building: '1'};
+        const response = schedule.showFilteredSchedule(filter);
+        expect(response).toEqual(mockSchedule);
+    });
+});
